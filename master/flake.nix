94d33cc374662508db28ba6886b0fb416dca40ae
hugosenari <hugosenari@gmail.com>
2022-03-27 21:53:40 -0300

    chore: re index nimrod-glfw

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-glfw-master.ref   = "refs/heads/master";
   inputs.src-glfw-master.owner = "rafaelvasco";
   inputs.src-glfw-master.repo  = "nimrod-glfw";
-  inputs.src-glfw-master.dir   = "";
   inputs.src-glfw-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
